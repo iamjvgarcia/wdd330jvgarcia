2aa2e37a7091ecba36dc9939aa18e7116f4c4e60
Jill Villoso Garcia <akirakira013@gmail.com>
2021-09-19 01:04:52 +0800

    css added

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
   listWeeklyItems(links,"weeklist")
 
   function listWeeklyItems(weekitems, listElementName)
-    let ol = document.getElementById(listElementName);
+    let ol = document.getElementById('weeklist');
     if(ol) {
         weekitems.forEach(element => {
-            let anchor = document.createElement('a')
-            anchor.innerHTML = element.label;
-            anchor.href = element.url;
+            let a = document.createElement('a')
+            a.innerHTML = element.label;
+            a.href = element.url;
 
             let li = document.createElement('li')
-            li.appendChild(anchor);
+            li.appendChild(a);
 
             ol.appendChild(li);
         });
