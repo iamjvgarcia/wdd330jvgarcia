a9f06f26abb492bcbc6d9fd65556a86be6a73bce
Jill Villoso Garcia <akirakira013@gmail.com>
2021-10-31 03:59:30 +0800

    save

--- week7/activity/hikes.js
+++ week7/activity/hikes.js
@@ -1,74 +1,268 @@
+
 //create an array of hikes
 const hikeList = [
   {
-    name: "Bechler Falls",
-    imgSrc: "falls.jpg",
-    imgAlt: "Image of Bechler Falls",
-    distance: "3 miles",
-    difficulty: "Easy",
+    name: 'Bechler Falls',
+    imgSrc: 'falls.jpg',
+    imgAlt: 'Image of Bechler Falls',
+    distance: '3 miles',
+    difficulty: 'Easy',
     description:
-      "Beautiful short hike along the Bechler river to Bechler Falls",
+      'Beautiful short hike along the Bechler river to Bechler Falls',
     directions:
-      "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead."
+      'Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead.'
   },
   {
-    name: "Teton Canyon",
-    imgSrc: "falls.jpg",
-    imgAlt: "Image of Bechler Falls",
-    distance: "3 miles",
-    difficulty: "Easy",
-    description: "Beautiful short (or long) hike through Teton Canyon.",
+    name: 'Teton Canyon',
+    imgSrc: 'falls.jpg',
+    imgAlt: 'Image of Bechler Falls',
+    distance: '3 miles',
+    difficulty: 'Easy',
+    description: 'Beautiful short (or long) hike through Teton Canyon.',
     directions:
-      "Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead."
+      'Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead.'
   },
   {
-    name: "Denanda Falls",
-    imgSrc: "falls.jpg",
-    imgAlt: "Image of Bechler Falls",
-    distance: "7 miles",
-    difficulty: "Moderate",
+    name: 'Denanda Falls',
+    imgSrc: 'falls.jpg',
+    imgAlt: 'Image of Bechler Falls',
+    distance: '7 miles',
+    difficulty: 'Moderate',
     description:
-      "Beautiful hike through Bechler meadows river to Denanda Falls",
+      'Beautiful hike through Bechler meadows river to Denanda Falls',
     directions:
-      "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead."
+      'Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead.'
   }
 ];
 
-const imgBasePath = "//byui-cit.github.io/cit261/examples/";
-//on load grab the array and insert it into the page
-window.addEventListener("load", () => {
-  showHikeList();
-});
+const imgBasePath = '//byui-cit.github.io/cit261/examples/';
+//on load grab the array and insert it into the page on load
 
-function showHikeList() {
-  const hikeListElement = document.getElementById("hikes");
-  hikeListElement.innerHTML = "";
-  renderHikeList(hikeList, hikeListElement);
+class Hikes {
+  constructor(elementId) {
+    this.parentElement = document.getElementById(elementId);
+    // we need a back button to return back to the list. This will build it and hide it. When we need it we just need to remove the 'hidden' class
+    this.backButton = this.buildBackButton();
+    this.comments = new Comments('hikes', 'comments');
+  }
+  // why is this function necessary?  hikeList is not exported, and so it cannot be seen outside of this module. I added this in case I ever need the list of hikes outside of the module. This also sets me up nicely if my data were to move. I can just change this method to the new source and everything will still work if I only access the data through this getter.
+  getAllHikes() {
+    return hikeList;
+  }
+  // For the first stretch we will need to get just one hike.
+  getHikeByName(hikeName) {
+    return this.getAllHikes().find(hike => hike.name === hikeName);
+  }
+  //show a list of hikes in the parentElement
+  showHikeList() {
+    this.parentElement.innerHTML = '';
+    // notice that we use our getter above to grab the list instead of getting it directly...this makes it easier on us if our data source changes...
+    renderHikeList(this.parentElement, this.getAllHikes());
+    this.addHikeListener();
+    // make sure the back button is hidden
+    this.backButton.classList.add('hidden');
+    this.comments.showCommentList();
+  }
+  // show one hike with full details in the parentElement
+  showOneHike(hikeName) {
+    const hike = this.getHikeByName(hikeName);
+    this.parentElement.innerHTML = '';
+    this.parentElement.appendChild(renderOneHikeFull(hike));
+    // show the back button
+    this.backButton.classList.remove('hidden');
+    // show the comments for just this hike
+    this.comments.showCommentList(hikeName);
+  }
+  // in order to show the details of a hike ontouchend we will need to attach a listener AFTER the list of hikes has been built. The function below does that.
+  addHikeListener() {
+    // We need to loop through the children of our list and attach a listener to each, remember though that children is a nodeList...not an array. So in order to use something like a forEach we need to convert it to an array.
+    const childrenArray = Array.from(this.parentElement.children);
+    childrenArray.forEach(child => {
+      child.addEventListener('touchend', e => {
+        // why currentTarget instead of target?
+        this.showOneHike(e.currentTarget.dataset.name);
+      });
+    });
+  }
+  buildBackButton() {
+    const backButton = document.createElement('button');
+    backButton.innerHTML = '&lt;- All Hikes';
+    backButton.addEventListener('touchend', () => {
+      this.showHikeList();
+    });
+    backButton.classList.add('hidden');
+    this.parentElement.before(backButton);
+    return backButton;
+  }
 }
-
-function renderHikeList(hikes, parent) {
+// methods responsible for building HTML.  Why aren't these in the class?  They don't really need to be, and by moving them outside of the exported class, they cannot be called outside the module...they become private.
+function renderHikeList(parent, hikes) {
   hikes.forEach(hike => {
-    parent.appendChild(renderHike(hike));
+    parent.appendChild(renderOneHikeLight(hike));
   });
 }
-function renderHike(hike) {
-  const item = document.createElement("li");
-
+function renderOneHikeLight(hike) {
+  const item = document.createElement('li');
+  item.classList.add('light');
+  // setting this to make getting the details for a specific hike easier later.
+  item.setAttribute('data-name', hike.name);
   item.innerHTML = ` <h2>${hike.name}</h2>
-        <div class="hikeContainer">
-        <div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
+<div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${
+    hike.imgAlt
+  }"></div>
+<div>
         <div>
-                <div>
-                    <h3>Distance</h3>
-                    <p>${hike.distance}</p>
-
-                    <h3>Difficulty</h3>
-                    <p>${hike.difficulty}</p>
-                </div>
+            <h3>Distance</h3>
+            <p>${hike.distance}</p>
         </div>
-        <div>`;
+        <div>
+            <h3>Difficulty</h3>
+            <p>${hike.difficulty}</p>
+        </div>
+</div>`;
 
   return item;
+}
+function renderOneHikeFull(hike) {
+  const item = document.createElement('li');
+  item.innerHTML = ` 
+    
+        <img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}">
+        <h2>${hike.name}</h2>
+        <div>
+            <h3>Distance</h3>
+            <p>${hike.distance}</p>
+        </div>
+        <div>
+            <h3>Difficulty</h3>
+            <p>${hike.difficulty}</p>
+        </div>
+        <div>
+            <h3>Description</h3>
+            <p>${hike.description}</p>
+        </div>
+        <div>
+            <h3>How to get there</h3>
+            <p>${hike.directions}</p>
+        </div>
+    
+    `;
+  return item;
+}
+
+// A the code that is responsible to get and set data for a part of the application is often called a model. I've used that terminology here.
+
+//commentModel
+class CommentModel {
+  constructor(type) {
+    this.type = type;
+    // get the initial list of comments out of local storage if it exists
+    this.comments = readFromLS(this.type) || [];
+  }
+  // I decided I could combine my getAllComments, and filterCommentsByName methods into one by passing in an optional query argument
+  getComments(q = null) {
+    if (q === null) {
+      // no query, get all comments of the type
+      return this.comments;
+    } else {
+      // comments for a specific post...filter by name
+      return this.comments.filter(el => el.name === q);
+    }
+  }
+
+  addComment(postName, comment) {
+    const newComment = {
+      name: postName,
+      comment: comment,
+      date: new Date()
+    };
+    this.comments.push(newComment);
+    writeToLS(this.type, this.comments);
+  }
+}
+
+function writeToLS(key, data) {
+  // we can use JSON.stringify to convert our object to a string that can be stored in localStorage.
+  window.localStorage.setItem(key, JSON.stringify(data));
+}
+
+function readFromLS(key) {
+  // the string we retrieve from localStorage needs to be converted back to an object with JSON.parse
+  return JSON.parse(window.localStorage.getItem(key));
+}
+
+// These methods create the HTML that is needed to output our list of comments to the screen.  Anything dealing with output to the browser is catagorized as view code.
+
+const commentUI = `<div class="addComment">
+<h2>Add a comment</h2>
+<input type="text" id="commentEntry" />
+<button id="commentSubmit">Comment</button>
+</div>
+<h2>Comments</h2>
+<ul class="comments"></ul>`;
+// I only had one function for the view...so I chose not to use an object or class.
+function renderCommentList(element, comments) {
+  // clear out any comments that might be listed
+  element.innerHTML = '';
+  // add the new list of comments
+  comments.forEach(el => {
+    let item = document.createElement('li');
+    item.innerHTML = `
+            ${el.name}: ${el.comment}
+      `;
+
+    element.appendChild(item);
+  });
+}
+
+// Comments: this code handles getting the list of comments from the data source, and outputting them to the screen at the right time.  This is often catagorized as Controller code.
+
+class Comments {
+  constructor(type, commentElementId) {
+    this.type = type;
+    this.commentElementId = commentElementId;
+    this.model = new CommentModel(this.type);
+  }
+
+  addSubmitListener(postName) {
+    // use element.ontouchend to avoid more than one listener getting attached at a time to the button.
+    document.getElementById('commentSubmit').ontouchend = () => {
+      // debugger;
+      this.model.addComment(
+        postName,
+        document.getElementById('commentEntry').value
+      );
+      document.getElementById('commentEntry').value = '';
+      this.showCommentList(postName);
+    };
+  }
+  showCommentList(q = null) {
+    try {
+      const parent = document.getElementById(this.commentElementId);
+      if (!parent) throw new Error('comment parent not found');
+      // check to see if the commentUI code has been added yet
+      if (parent.innerHTML === '') {
+        parent.innerHTML = commentUI;
+      }
+      if (q !== null) {
+        // looking at one post, show comments and new comment button
+        document.querySelector('.addComment').style.display = 'block';
+        this.addSubmitListener(q);
+      } else {
+        // no post name provided, hide comment entry
+        document.querySelector('.addComment').style.display = 'none';
+      }
+      // get the comments from the model
+      let comments = this.model.getComments(q);
+      if (comments === null) {
+        // avoid an error if there are no comments yet.
+        comments = [];
+      }
+      renderCommentList(parent.lastChild, comments);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 
