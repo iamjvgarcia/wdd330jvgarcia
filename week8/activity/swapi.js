6d5b87e8b8e977976eeae855bda342e3dfdde553
Jill Villoso Garcia <akirakira013@gmail.com>
2021-11-07 06:51:29 +0800

    edit

--- week8/activity/swapi.js
+++ week8/activity/swapi.js
@@ -1,23 +1,98 @@
-const loadJson = () => {
-    return fetch('movies.json')
-      .then((response) => {
-        loadMenu();
-        return response.json()
-      })
-      .then((res) => {
-        const moviesList = document.querySelector('ul');
-        for (let i = 0; i < res.movies.length; i++) {
-          const movie = document.createElement('li');
-          movie.innerHTML = `<h3>${res.movies[ i ].title}</h3>`;
-          movie.innerHTML += `<p> Episode ${res.movies[ i ].episode_number}</p>`;
-          movie.innerHTML += `<p> ${res.movies[ i ].description}</p>`;
-          let castList = '<ul>';
-          res.movies[ i ].main_characters.forEach((character) => {
-            castList += `<li><img src="./falcon.png" class="falcon">${character}</li>`;
-          })
-          castList += '</ul>'
-          movie.innerHTML += ` Cast: ${castList}`;
-          moviesList.appendChild(movie);
-        }
-      })
-  }
+var next = null
+var previous = null
+
+window.onload = function () {
+    makeRequest("https://swapi.dev/api/people/").then(data => {
+        console.log(data)
+        displayPersons(data)
+    })
+}
+
+document.getElementById("previousButton").addEventListener("click", () => {
+    let display = document.getElementById("people")
+
+    if (previous != null) {
+        makeRequest(previous).then(data => {
+            display.innerHTML = ""
+            displayPersons(data)
+        })
+    }
+})
+
+document.getElementById("nextButton").addEventListener("click", () => {
+    let display = document.getElementById("people")
+
+    if (next != null) {
+        makeRequest(next).then(data => {
+            display.innerHTML = ""
+            displayPersons(data)
+        })
+    }
+})
+
+
+function displayPersons(people) {
+    let display = document.querySelector("#people")
+
+    if (people.next != null) {
+        let url = new URL(people.next)
+        url.protocol = "https:"
+        next = url.toString()
+    }
+
+    if (people.previous != null) {
+        let purl = new URL(people.previous)
+        purl.protocol = "https:"
+        previous = purl.toString()
+    }
+
+    people.results.forEach(person => {
+
+        let newPerson = document.createElement("div")
+        newPerson.className = 'person'
+        newPerson.id = person.url
+        newPerson.innerHTML += `<div class="personName">${person.name}</div>`
+
+        newPerson.addEventListener("click", () => showMoreDetails(newPerson))
+
+        display.appendChild(newPerson)
+    })
+}
+
+
+
+async function showMoreDetails(person) {
+    let element = document.getElementById(person.id)
+    console.log(element.children.length)
+    if (element.children.length > 1) {
+        let removeMe = document.getElementById(`details${person.id}`)
+        removeMe.remove()
+
+        return
+    }
+
+
+    let url = new URL(person.id)
+
+    url.protocol = 'https:'
+
+    makeRequest(url.toString()).then(details => {
+        console.log(details)
+        element.innerHTML += `<div id='details${person.id}' class='details'> 
+            <div>height = ${details.height}</div>
+            <div>mass = ${details.mass}</div>
+            <div>hair color = ${details.hair_color}</div>
+            <div>skin color = ${details.skin_color}</div>
+            <div>eye color = ${details.eye_color}</div>
+            <div>birth year = ${details.birth_year}</div>
+            <div>gender = ${details.gender}</div>
+            </div>`
+    })
+}
+
+async function makeRequest(url) {
+    let response = await fetch(url)
+    let data = response.json()
+
+    return data
+}
