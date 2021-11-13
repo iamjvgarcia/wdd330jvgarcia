057a775c39a213a54b40484c72f6a77d003e13f1
Jill Villoso Garcia <akirakira013@gmail.com>
2021-11-14 02:35:32 +0800

    added week 9 to main directory

--- js/main.js
+++ js/main.js
@@ -32,6 +32,10 @@ const links = [
       label: "Week8 notes",
       url: "week8/index.html",
     } ,
+    {
+      label: "Week9 notes",
+      url: "week9/index.html",
+    } ,
 ];
 
 const populateContents = () => {
