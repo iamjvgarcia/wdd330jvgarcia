cfb200e720b9103dac871fcd2153f734528f1170
Jill Villoso Garcia <akirakira013@gmail.com>
2021-11-20 20:05:50 +0800

    add week 10

--- js/main.js
+++ js/main.js
@@ -36,6 +36,10 @@ const links = [
       label: "Week9 notes",
       url: "week9/index.html",
     } ,
+    {
+      label: "Week10 notes",
+      url: "week10/index.html",
+    } ,
 ];
 
 const populateContents = () => {
